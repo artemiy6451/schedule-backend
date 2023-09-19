4f27f7bcd68e44efd98dce9becf76d07c57f97ea
Kazantsev Artem <me@lokach.ru>
2023-09-19 16:40:52 +0000

    feat: add tests for structure endpoints

--- app/structure/services.py
+++ app/structure/services.py
@@ -1,7 +1,9 @@
 """Файл с сервисами структурного подразделения."""
 
+from sqlalchemy.exc import IntegrityError, NoResultFound
+
 from app.database import async_session_maker
-from app.exceptions import ItemNotFoundError
+from app.exceptions import ItemAlreadyExistError, ItemNotFoundError, UniversalError
 from app.repository import AbstractRepository
 from app.services import Service
 from app.structure.schemas import StructureSchema, StructureSchemaAdd
@@ -16,9 +18,16 @@ class StructureService(Service):
 
     async def add(self, structure: StructureSchemaAdd) -> int:
         """Метод для добавления структурного подразделения."""
-        structure_dict = structure.model_dump()
-        id = await self.repository.add_one(structure_dict)
-        return id
+        try:
+            structure_dict = structure.model_dump()
+            id = await self.repository.add_one(structure_dict)
+            return id
+        except IntegrityError as e:
+            if "UniqueViolationError" in e.orig.__str__():
+                raise ItemAlreadyExistError("Item already exist.") from None
+            raise UniversalError(e) from e
+        except Exception as e:
+            raise UniversalError(e) from e
 
     async def get_all(self) -> list[StructureSchema]:
         """Метод для получения всех структурных подразделений."""
@@ -36,5 +45,10 @@ class StructureService(Service):
 
     async def delete(self, id: int) -> int:
         """Метод для удаления структурного подразделения."""
-        res: int = await self.repository.delete_one(id=id)
-        return res
+        try:
+            res: int = await self.repository.delete_one(id=id)
+            return res
+        except NoResultFound:
+            raise ItemNotFoundError("Item not found.") from NoResultFound
+        except Exception:
+            raise UniversalError from Exception
